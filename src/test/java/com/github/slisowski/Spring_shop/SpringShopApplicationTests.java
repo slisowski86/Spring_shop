eaca6e57dfbe79e73019088f93365d1c96363f87
Slawomir Lisowski <slisowski@gmail.com>
2020-09-15 06:17:39 +0100

    Maven configured

---
+++ src/test/java/com/github/slisowski/Spring_shop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.slisowski.Spring_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
