2571c8ec2cf72d2597ba429ff0d198ac9328bfb9
Slawek Lisowski <slisowski@gmail.com>
2021-01-07 07:11:26 +0100

    One layout

--- src/main/java/com/github/slisowski/Spring_shop/controller/ShoppingListController.java
+++ src/main/java/com/github/slisowski/Spring_shop/controller/ShoppingListController.java
@@ -10,12 +10,10 @@ import com.github.slisowski.Spring_shop.model.projection.ListWriteModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
